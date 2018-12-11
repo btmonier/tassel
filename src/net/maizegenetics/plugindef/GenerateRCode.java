75a1e6bed03db1ec44ccc32509d6f394c085368e
Terry Casstevens <tmc46@cornell.edu>
2018-12-11 09:57:19 -0500

    TAS-1292 Updated GenerateRCode

--- src/net/maizegenetics/plugindef/GenerateRCode.java
+++ src/net/maizegenetics/plugindef/GenerateRCode.java
@@ -18,7 +18,6 @@ import org.apache.log4j.Logger;
 import java.awt.*;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -35,9 +34,9 @@ public class GenerateRCode {
 
     public static void main(String[] args) {
         printHeader();
-        generate(FilterSiteBuilderPlugin.class, "genotypeTable");
-        generate(FilterTaxaBuilderPlugin.class, "genotypeTable");
-        generate(KinshipPlugin.class, "genotypeTable");
+        generate(FilterSiteBuilderPlugin.class, "genotypeTable", "genotypeTable");
+        generate(FilterTaxaBuilderPlugin.class, "genotypeTable", "genotypeTable");
+        generate(KinshipPlugin.class, "genotypeTable", "distanceMatrix");
     }
 
     private static void printHeader() {
@@ -53,7 +52,7 @@ public class GenerateRCode {
         System.out.println("#--------------------------------------------------------------------");
 
         System.out.println("# Preamble\n");
-        
+
         System.out.println("\n## Load packages");
         System.out.println("if (!requireNamespace(\"BiocManager\")) {");
         System.out.println("    install.packages(\"BiocManager\")");
@@ -72,18 +71,18 @@ public class GenerateRCode {
         System.out.println("rJava::.jaddClassPath(\"/tassel-5-standalone/lib\")");
         System.out.println("rJava::.jaddClassPath(\"/tassel-5-standalone/sTASSEL.jar\")\n");
 
-        System.out.println("source(\"R/AllClasses.R\")");
+        System.out.println("source(\"R/AllClasses.R\")\n");
 
     }
 
-    public static void generate(Class currentMatch, String inputObject) {
+    public static void generate(Class currentMatch, String inputObject, String outputObject) {
         try {
             Constructor constructor = currentMatch.getConstructor(Frame.class);
-            generate((AbstractPlugin) constructor.newInstance((Frame) null), inputObject);
+            generate((AbstractPlugin) constructor.newInstance((Frame) null), inputObject, outputObject);
         } catch (Exception ex) {
             try {
                 Constructor constructor = currentMatch.getConstructor(Frame.class, boolean.class);
-                generate((AbstractPlugin) constructor.newInstance(null, false), inputObject);
+                generate((AbstractPlugin) constructor.newInstance(null, false), inputObject, outputObject);
             } catch (NoSuchMethodException nsme) {
                 myLogger.warn("Self-describing Plugins should implement this constructor: " + currentMatch.getClass().getName());
                 myLogger.warn("public Plugin(Frame parentFrame, boolean isInteractive) {");
@@ -95,7 +94,7 @@ public class GenerateRCode {
         }
     }
 
-    private static void generate(AbstractPlugin plugin, String inputObject) {
+    private static void generate(AbstractPlugin plugin, String inputObject, String outputObject) {
 
         String clazz = Utils.getBasename(plugin.getClass().getName());
         //StringBuilder sb = new StringBuilder("rTASSEL::" + stringToCamelCase(clazz));
@@ -121,6 +120,13 @@ public class GenerateRCode {
         sb.deleteCharAt(sb.lastIndexOf(",")); //remove last comma
 
         sb.append(") {\n");
+
+        if (inputObject.equals("genotypeTable")) {
+            sb.append("    if(is(genotypeTable, \"GenotypeTable\") == TRUE) {\n");
+            sb.append("        genotypeTable <- genotypeTable@jtsGenotypeTable\n");
+            sb.append("    }\n");
+        }
+
         sb.append("    plugin <- new(J(\"" + plugin.getClass().getCanonicalName() + "\"), .jnull(), FALSE)\n");
         //sb.append("    plugin <- rJava::.jnew(\"" + plugin.getClass().getCanonicalName() + "\")\n");
         for (Field field : plugin.getParameterFields()) {
@@ -139,6 +145,15 @@ public class GenerateRCode {
             }
         }
         sb.append("    plugin$runPlugin(" + inputObject + ")\n");
+
+        if (outputObject.equals("genotypeTable")) {
+            sb.append("    new(\n");
+            sb.append("        Class = \"GenotypeTable\",\n");
+            sb.append("        name = paste0(\"Filtered:\"),\n");
+            sb.append("        jtsGenotypeTable = filteredGT\n");
+            sb.append("    )\n");
+        }
+
         sb.append("}\n");
         System.out.println(sb.toString());
 
@@ -195,30 +210,32 @@ public class GenerateRCode {
 
     /**
      * Temporary place for this experimental method.
+     *
      * @param genotype
+     *
      * @return int[] in column order with NA set to R approach
      */
     public static int[] genotypeTableToDosageIntArray(GenotypeTable genotype) {
 
-        int[] result = new int[genotype.numberOfTaxa()*genotype.numberOfSites()];
+        int[] result = new int[genotype.numberOfTaxa() * genotype.numberOfSites()];
 
-        int index=0;
-        for (int site=0; site< genotype.numberOfSites(); site++) {
+        int index = 0;
+        for (int site = 0; site < genotype.numberOfSites(); site++) {
             byte[] siteGenotypes = genotype.genotypeAllTaxa(site);
             int[][] alleleCounts = AlleleFreqCache.allelesSortedByFrequencyNucleotide(siteGenotypes);
             byte majorAllele = AlleleFreqCache.majorAllele(alleleCounts);
             // value assigned to site / taxon is the number of alleles
             // that doesn't not match the major allele.
-            for (int taxon=0; taxon< genotype.numberOfTaxa(); taxon++) {
+            for (int taxon = 0; taxon < genotype.numberOfTaxa(); taxon++) {
                 int value = 0;
                 byte[] alleles = GenotypeTableUtils.getDiploidValues(siteGenotypes[taxon]);
-                if(alleles[0]==GenotypeTable.UNKNOWN_ALLELE || alleles[1]==GenotypeTable.UNKNOWN_ALLELE) {
+                if (alleles[0] == GenotypeTable.UNKNOWN_ALLELE || alleles[1] == GenotypeTable.UNKNOWN_ALLELE) {
                     value = Integer.MIN_VALUE;
                 } else {
                     if (alleles[0] != majorAllele) value++;
                     if (alleles[1] != majorAllele) value++;
                 }
-                result[index++]=value;
+                result[index++] = value;
             }
         }
         return result;
@@ -226,40 +243,44 @@ public class GenerateRCode {
 
     /**
      * Temporary place for this experimental method.
+     *
      * @param genotype
+     *
      * @return int[] in column order with NA set to R approach
      */
     public static String[] genotypeTableToSampleNameArray(GenotypeTable genotype) {
         return genotype.taxa().stream()
-            .map(Taxon::getName)
+                .map(Taxon::getName)
                 .toArray(String[]::new);
     }
 
     /**
      * Temporary place for this experimental method.
+     *
      * @param genotype
+     *
      * @return int[] in column order with NA set to R approach
      */
     public static PositionVectors genotypeTableToPositionListOfArrays(GenotypeTable genotype) {
 
-        String[] chromosomes=new String[genotype.numberOfSites()];
-        int[] startPos=new int[genotype.numberOfSites()];
-        int[] strand=new int[genotype.numberOfSites()];
-        String[] refAllele=new String[genotype.numberOfSites()];
-        String[] altAllele=new String[genotype.numberOfSites()];
+        String[] chromosomes = new String[genotype.numberOfSites()];
+        int[] startPos = new int[genotype.numberOfSites()];
+        int[] strand = new int[genotype.numberOfSites()];
+        String[] refAllele = new String[genotype.numberOfSites()];
+        String[] altAllele = new String[genotype.numberOfSites()];
 
-        for (int site=0; site< genotype.numberOfSites(); site++) {
-            Position p=genotype.positions().get(site);
-            chromosomes[site]=p.getChromosome().getName();
-            startPos[site]=p.getPosition();
-            strand[site]=p.getStrand();
-            String[] variants=p.getKnownVariants();
-            refAllele[site]=(variants.length>0)?variants[0]:"";
-            altAllele[site]=(variants.length>1)?variants[1]:"";
+        for (int site = 0; site < genotype.numberOfSites(); site++) {
+            Position p = genotype.positions().get(site);
+            chromosomes[site] = p.getChromosome().getName();
+            startPos[site] = p.getPosition();
+            strand[site] = p.getStrand();
+            String[] variants = p.getKnownVariants();
+            refAllele[site] = (variants.length > 0) ? variants[0] : "";
+            altAllele[site] = (variants.length > 1) ? variants[1] : "";
         }
 
-       // return startPos;
-        return new PositionVectors(chromosomes,startPos,strand,refAllele,altAllele);
+        // return startPos;
+        return new PositionVectors(chromosomes, startPos, strand, refAllele, altAllele);
 //        //This can be replace with a custom class in the future or List.of in Java 9
 //        java.util.List list=new ArrayList<>();
 //        list.add(chromosomes);
