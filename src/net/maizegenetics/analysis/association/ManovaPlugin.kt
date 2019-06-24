603d6b74de71ccf4f088f7cffdd408125140b467
Terry Casstevens <tmc46@cornell.edu>
2019-06-24 11:25:59 -0400

    Add ManovaPlugin

---
+++ src/net/maizegenetics/analysis/association/ManovaPlugin.kt
@@ -0,0 +1,429 @@
+package net.maizegenetics.analysis.association
+
+import net.maizegenetics.dna.snp.GenotypeTable
+import net.maizegenetics.plugindef.AbstractPlugin
+import net.maizegenetics.plugindef.DataSet
+import net.maizegenetics.plugindef.PluginParameter
+import org.apache.log4j.Logger
+import java.awt.Frame
+import javax.swing.ImageIcon
+
+/**
+ * @author Terry Casstevens
+ * Created June 24, 2019
+ */
+
+class ManovaPlugin(parentFrame: Frame?, isInteractive: Boolean) : AbstractPlugin(parentFrame, isInteractive) {
+
+    private val myLogger = Logger.getLogger(ManovaPlugin::class.java)
+
+    private var usePermutations = PluginParameter.Builder("usePerm", true, Boolean::class.javaObjectType)
+            .description("Should permutations be used to set the enter and exit limits for stepwise regression? A permutation test will be used to determine the enter limit. The exit limit will be set to 2 times the enter limit.")
+            .guiName("Use permutations")
+            .build()
+
+    private var numberOfPermutations = PluginParameter.Builder("nPerm", 1000, Int::class.javaObjectType)
+            .description("The number of permutations used to determine the enter limit.")
+            .guiName("Number of permutations")
+            .dependentOnParameter(usePermutations)
+            .build()
+
+    private var enterLimit = PluginParameter.Builder("enterLimit", 1e-5, Double::class.javaObjectType)
+            .description("When p-value is the model selection criteria, model fitting will stop when the next term chosen has a p-value greater than the enterLimit. This value will be over-ridden the permutation test, if used.")
+            .guiName("enterLimit")
+            .dependentOnParameter(usePermutations, false)
+            .build()
+
+    private var exitLimit = PluginParameter.Builder("exitLimit", 2e-5, Double::class.javaObjectType)
+            .description("During the backward step of model fitting if p-value has been chosen as the selection criterion, if the term in model with the highest p-value has a p-value > exitLimit, it will be removed from the model.")
+            .guiName("exitLimit")
+            .dependentOnParameter(usePermutations, false)
+            .build()
+
+    private var isNested = PluginParameter.Builder("isNested", true, Boolean::class.javaObjectType)
+            .description("Should SNPs/markers be nested within a factor, such as family?")
+            .guiName("")
+            .build()
+
+    private var nestingFactor = PluginParameter.Builder("nestFactor", null, String::class.java)
+            .guiName("Nesting factor")
+            .description("Nest markers within this factor. This parameter cannot be set from the command line. Instead, the first factor in the data set will be used.")
+            .dependentOnParameter(isNested)
+            .objectListSingleSelect()
+            .build()
+
+    private var myGenotypeTable: PluginParameter<GenotypeTable.GENOTYPE_TABLE_COMPONENT> = PluginParameter.Builder("genotypeComponent", GenotypeTable.GENOTYPE_TABLE_COMPONENT.Genotype, GenotypeTable.GENOTYPE_TABLE_COMPONENT::class.java)
+            .genotypeTable()
+            .range(GenotypeTable.GENOTYPE_TABLE_COMPONENT.values())
+            .description("If the genotype table contains more than one type of genotype data, choose the type to use for the analysis.")
+            .build()
+
+    private var createAnova = PluginParameter.Builder("anova", true, Boolean::class.javaObjectType)
+            .description("Create pre- and post-scan anova reports.")
+            .guiName("Create anova reports")
+            .build()
+
+    private var createEffects = PluginParameter.Builder("effects", true, Boolean::class.javaObjectType)
+            .description("Create a report of marker effects based on the scan results.")
+            .guiName("Create effects report")
+            .build()
+
+    private var createStep = PluginParameter.Builder("step", true, Boolean::class.javaObjectType)
+            .description("Create a report of the which markers enter and leave the model as it is being fit.")
+            .guiName("Create step report")
+            .build()
+
+    private var createResiduals = PluginParameter.Builder("residuals", false, Boolean::class.javaObjectType)
+            .description("Create a phenotype dataset of model residuals for each chromosome. For each chromosome, the residuals will be calculated from a model with all terms EXCEPT the markers on that chromosome.")
+            .guiName("Create residuals")
+            .build()
+
+    private var writeFiles = PluginParameter.Builder("saveToFile", false, Boolean::class.javaObjectType)
+            .description("Should the requested output be written to files?")
+            .guiName("Write to files")
+            .build()
+
+    private var outputName = PluginParameter.Builder("savePath", "", String::class.java)
+            .description("The base file path for the save files. Each file saved will add a descriptive name to the base name.")
+            .guiName("Base file path")
+            .outFile()
+            .dependentOnParameter(writeFiles)
+            .build()
+
+    override fun processData(input: DataSet?): DataSet {
+        val temp = numberOfPermutations()
+        return super.processData(input)
+    }
+
+    override fun getIcon(): ImageIcon? {
+        return null
+    }
+
+    override fun getButtonName(): String {
+        return "Multi-trait Stepwise"
+    }
+
+    override fun getToolTipText(): String {
+        return "Multi-trait Stepwise"
+    }
+
+    /**
+     * Should permutations be used to set the enter and exit
+     * limits for stepwise regression? A permutation test
+     * will be used to determine the enter limit. The exit
+     * limit will be set to 2 times the enter limit.
+     *
+     * @return Use permutations
+     */
+    fun usePermutations(): Boolean? {
+        return usePermutations.value()
+    }
+
+    /**
+     * Set Use permutations. Should permutations be used to
+     * set the enter and exit limits for stepwise regression?
+     * A permutation test will be used to determine the enter
+     * limit. The exit limit will be set to 2 times the enter
+     * limit.
+     *
+     * @param value Use permutations
+     *
+     * @return this plugin
+     */
+    fun usePermutations(value: Boolean?): ManovaPlugin {
+        usePermutations = PluginParameter(usePermutations, value)
+        return this
+    }
+
+    /**
+     * The number of permutations used to determine the enter
+     * limit.
+     *
+     * @return Number of permutations
+     */
+    fun numberOfPermutations(): Int {
+        return numberOfPermutations.value()
+    }
+
+    /**
+     * Set Number of permutations. The number of permutations
+     * used to determine the enter limit.
+     *
+     * @param value Number of permutations
+     *
+     * @return this plugin
+     */
+    fun numberOfPermutations(value: Int): ManovaPlugin {
+        numberOfPermutations = PluginParameter(numberOfPermutations, value)
+        return this
+    }
+
+    /**
+     * When p-value is the model selection criteria, model
+     * fitting will stop when the next term chosen has a p-value
+     * greater than the enterLimit. This value will be over-ridden
+     * the permutation test, if used.
+     *
+     * @return enterLimit
+     */
+    fun enterLimit(): Double? {
+        return enterLimit.value()
+    }
+
+    /**
+     * Set enterLimit. When p-value is the model selection
+     * criteria, model fitting will stop when the next term
+     * chosen has a p-value greater than the enterLimit. This
+     * value will be over-ridden the permutation test, if
+     * used.
+     *
+     * @param value enterLimit
+     *
+     * @return this plugin
+     */
+    fun enterLimit(value: Double?): ManovaPlugin {
+        enterLimit = PluginParameter(enterLimit, value)
+        return this
+    }
+
+    /**
+     * During the backward step of model fitting if p-value
+     * has been chosen as the selection criterion, if the
+     * term in model with the highest p-value has a p-value
+     * > exitLimit, it will be removed from the model.
+     *
+     * @return exitLimit
+     */
+    fun exitLimit(): Double? {
+        return exitLimit.value()
+    }
+
+    /**
+     * Set exitLimit. During the backward step of model fitting
+     * if p-value has been chosen as the selection criterion,
+     * if the term in model with the highest p-value has a
+     * p-value > exitLimit, it will be removed from the model.
+     *
+     * @param value exitLimit
+     *
+     * @return this plugin
+     */
+    fun exitLimit(value: Double?): ManovaPlugin {
+        exitLimit = PluginParameter(exitLimit, value)
+        return this
+    }
+
+    /**
+     * Should SNPs/markers be nested within a factor, such
+     * as family?
+     *
+     * @return Is Nested
+     */
+    fun isNested(): Boolean? {
+        return isNested.value()
+    }
+
+    /**
+     * Set Is Nested. Should SNPs/markers be nested within
+     * a factor, such as family?
+     *
+     * @param value Is Nested
+     *
+     * @return this plugin
+     */
+    fun isNested(value: Boolean?): ManovaPlugin {
+        isNested = PluginParameter(isNested, value)
+        return this
+    }
+
+    /**
+     * Nest markers within this factor. This parameter cannot
+     * be set from the command line. Instead, the first factor
+     * in the data set will be used.
+     *
+     * @return Nesting factor
+     */
+    fun nestingFactor(): String {
+        return nestingFactor.value()
+    }
+
+    /**
+     * Set Nesting factor. Nest markers within this factor.
+     * This parameter cannot be set from the command line.
+     * Instead, the first factor in the data set will be used.
+     *
+     * @param value Nesting factor
+     *
+     * @return this plugin
+     */
+    fun nestingFactor(value: String): ManovaPlugin {
+        nestingFactor = PluginParameter(nestingFactor, value)
+        return this
+    }
+
+    /**
+     * If the genotype table contains more than one type of
+     * genotype data, choose the type to use for the analysis.
+     *
+     * @return Genotype Component
+     */
+    fun genotypeTable(): GenotypeTable.GENOTYPE_TABLE_COMPONENT {
+        return myGenotypeTable.value()
+    }
+
+    /**
+     * Set Genotype Component. If the genotype table contains
+     * more than one type of genotype data, choose the type
+     * to use for the analysis.
+     *
+     * @param value Genotype Component
+     *
+     * @return this plugin
+     */
+    fun genotypeTable(value: GenotypeTable.GENOTYPE_TABLE_COMPONENT): ManovaPlugin {
+        myGenotypeTable = PluginParameter(myGenotypeTable, value)
+        return this
+    }
+
+    /**
+     * Create pre- and post-scan anova reports.
+     *
+     * @return Create anova reports
+     */
+    fun createAnova(): Boolean? {
+        return createAnova.value()
+    }
+
+    /**
+     * Set Create anova reports. Create pre- and post-scan
+     * anova reports.
+     *
+     * @param value Create anova reports
+     *
+     * @return this plugin
+     */
+    fun createAnova(value: Boolean?): ManovaPlugin {
+        createAnova = PluginParameter(createAnova, value)
+        return this
+    }
+
+    /**
+     * Create a report of marker effects based on the scan
+     * results.
+     *
+     * @return Create effects report
+     */
+    fun createEffects(): Boolean? {
+        return createEffects.value()
+    }
+
+    /**
+     * Set Create effects report. Create a report of marker
+     * effects based on the scan results.
+     *
+     * @param value Create effects report
+     *
+     * @return this plugin
+     */
+    fun createEffects(value: Boolean?): ManovaPlugin {
+        createEffects = PluginParameter(createEffects, value)
+        return this
+    }
+
+    /**
+     * Create a report of the which markers enter and leave
+     * the model as it is being fit.
+     *
+     * @return Create step report
+     */
+    fun createStep(): Boolean? {
+        return createStep.value()
+    }
+
+    /**
+     * Set Create step report. Create a report of the which
+     * markers enter and leave the model as it is being fit.
+     *
+     * @param value Create step report
+     *
+     * @return this plugin
+     */
+    fun createStep(value: Boolean?): ManovaPlugin {
+        createStep = PluginParameter(createStep, value)
+        return this
+    }
+
+    /**
+     * Create a phenotype dataset of model residuals for each
+     * chromosome. For each chromosome, the residuals will
+     * be calculated from a model with all terms EXCEPT the
+     * markers on that chromosome.
+     *
+     * @return Create residuals
+     */
+    fun createResiduals(): Boolean? {
+        return createResiduals.value()
+    }
+
+    /**
+     * Set Create residuals. Create a phenotype dataset of
+     * model residuals for each chromosome. For each chromosome,
+     * the residuals will be calculated from a model with
+     * all terms EXCEPT the markers on that chromosome.
+     *
+     * @param value Create residuals
+     *
+     * @return this plugin
+     */
+    fun createResiduals(value: Boolean?): ManovaPlugin {
+        createResiduals = PluginParameter(createResiduals, value)
+        return this
+    }
+
+    /**
+     * Should the requested output be written to files?
+     *
+     * @return Write to files
+     */
+    fun writeFiles(): Boolean? {
+        return writeFiles.value()
+    }
+
+    /**
+     * Set Write to files. Should the requested output be
+     * written to files?
+     *
+     * @param value Write to files
+     *
+     * @return this plugin
+     */
+    fun writeFiles(value: Boolean?): ManovaPlugin {
+        writeFiles = PluginParameter(writeFiles, value)
+        return this
+    }
+
+    /**
+     * The base file path for the save files. Each file saved
+     * will add a descriptive name to the base name.
+     *
+     * @return Base file path
+     */
+    fun outputName(): String {
+        return outputName.value()
+    }
+
+    /**
+     * Set Base file path. The base file path for the save
+     * files. Each file saved will add a descriptive name
+     * to the base name.
+     *
+     * @param value Base file path
+     *
+     * @return this plugin
+     */
+    fun outputName(value: String): ManovaPlugin {
+        outputName = PluginParameter(outputName, value)
+        return this
+    }
+
+}
