4fe01516d294fcda37d5ff6a70518f2bb7ceba24
peter bradbury <pjb39@cornell.edu>
2019-07-02 15:12:41 -0400

    adds NestedFactorModelEffect

--- src/net/maizegenetics/stats/linearmodels/ModelEffects.kt
+++ src/net/maizegenetics/stats/linearmodels/ModelEffects.kt
@@ -1,57 +1,88 @@
 package net.maizegenetics.stats.linearmodels
 
 import net.maizegenetics.matrixalgebra.Matrix.DoubleMatrix
+import net.maizegenetics.matrixalgebra.Matrix.DoubleMatrixFactory
 
-class NestedFactorModelEffect(nestedEffect : FactorModelEffect, withinEffect : FactorModelEffect, id : Any) : ModelEffect {
+class NestedFactorModelEffect(val nestedEffect : FactorModelEffect, val outerEffect : FactorModelEffect, val id : Any) : ModelEffect {
+    //so that the designMatrix is only calculated once
+    var designMatrix : DoubleMatrix? = null
+
+    init {
+        if (nestedEffect.size != outerEffect.size) throw IllegalArgumentException("nested and outer effects must have the same size (number of observations)")
+    }
+
     override fun getID(): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return id
     }
 
     override fun setID(id: Any?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        throw NotImplementedError("setId is not implemented and must be set in the constructor.")
     }
 
     override fun getSize(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return nestedEffect.getSize()
     }
 
     override fun getX(): DoubleMatrix {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        if (designMatrix == null) {
+            //do element-wise multiplication of columns of nestedEffect with columns of outerEffect
+            val nestedDM = nestedEffect.x
+            val outerDM = outerEffect.x
+            val nrows = nestedDM.numberOfRows()
+            val ncols = nestedDM.numberOfColumns() * outerDM.numberOfColumns();
+
+            val designMatrix = DoubleMatrixFactory.DEFAULT.make(nrows, ncols)
+
+            var designCol = 0
+            for (nestedCol in 0 until nestedDM.numberOfColumns()) {
+                for (outerCol in 0 until outerDM.numberOfColumns()) {
+                    for (row in 0 until nrows) {
+                        designMatrix.set(row, designCol, nestedDM.get(row, nestedCol) * outerDM.get(row, outerCol))
+                    }
+                    designCol++
+                }
+            }
+
+        }
+        return designMatrix as DoubleMatrix
+
     }
 
     override fun getXtX(): DoubleMatrix {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return getX().crossproduct()
     }
 
     override fun getXty(y: DoubleArray?): DoubleMatrix {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if ( y!= null) return getX().mult(DoubleMatrixFactory.DEFAULT.make(y.size, 1, y))
+        else throw java.lang.IllegalArgumentException("null agrument to getXty not allowed")
     }
 
     override fun getyhat(beta: DoubleMatrix?): DoubleMatrix {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        throw NotImplementedError("getyhat not implemented for NestFactorModelEffects")
     }
 
     override fun getyhat(beta: DoubleArray?): DoubleMatrix {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        throw NotImplementedError("getyhat not implemented for NestFactorModelEffects")
     }
 
     override fun getLevelCounts(): IntArray {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        throw NotImplementedError("getyhat not implemented for NestFactorModelEffects")
     }
 
     override fun getNumberOfLevels(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        throw NotImplementedError("getLevelCounts not implemented for NestFactorModelEffects")
     }
 
     override fun getEffectSize(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        throw NotImplementedError("getEffectSize not implemented for NestFactorModelEffects")
     }
 
     override fun getCopy(): ModelEffect {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        throw NotImplementedError("getCopy not implemented for NestFactorModelEffects")
     }
 
     override fun getSubSample(sample: IntArray?): ModelEffect {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        throw NotImplementedError("getSubSample not implemented for NestFactorModelEffects")
     }
 }
