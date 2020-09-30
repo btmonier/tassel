4a07993b82bd44134a7184ef7f6d4ea964c217d6
lynnjo <lcj34@cornell.edu>
2020-09-30 10:56:03 -0400

    Added "tagFile" parameter to GetTagTaxaDistFromDBPlugin.

--- src/net/maizegenetics/analysis/gbs/v2/GetTagTaxaDistFromDBPlugin.java
+++ src/net/maizegenetics/analysis/gbs/v2/GetTagTaxaDistFromDBPlugin.java
@@ -4,24 +4,19 @@
 package net.maizegenetics.analysis.gbs.v2;
 
 import java.awt.Frame;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Set;
+import java.util.*;
 
 import javax.swing.ImageIcon;
 
+import net.maizegenetics.dna.tag.*;
 import org.apache.log4j.Logger;
 
-import net.maizegenetics.dna.tag.Tag;
-import net.maizegenetics.dna.tag.TagDataSQLite;
-import net.maizegenetics.dna.tag.TagDataWriter;
-import net.maizegenetics.dna.tag.TaxaDistribution;
 import net.maizegenetics.plugindef.AbstractPlugin;
 import net.maizegenetics.plugindef.DataSet;
+import net.maizegenetics.plugindef.GeneratePluginCode;
 import net.maizegenetics.plugindef.PluginParameter;
 import net.maizegenetics.taxa.TaxaList;
 
@@ -29,6 +24,14 @@ import net.maizegenetics.taxa.TaxaList;
  * This plugin takes a GBSv2 database as input, queries for the tags
  * and their taxa distribution, and creates a tab-delimited file of tag
  * and taxa-distribution.  IT can be used for verifying data in the db.
+ *
+ * The user may supply a file with tab-delimited columns specifying on which tags to present data.
+ * The input "tag" file must have at least 1 column named "TAGS" (any case - Tags/TAGS/tags).  Other
+ * data in the file will be ignored.
+ *
+ * Output:  A tab-delimited file where the first column is the tag, and subsequent columns
+ * indicate taxa found in the db.  The values in the "taxa" columns are the number of occurranes of
+ * that tag for that taxa.
  * 
  * @author lcj34
  *
@@ -40,6 +43,8 @@ public class GetTagTaxaDistFromDBPlugin extends AbstractPlugin {
             .description("Input database file with tags").build();
     private PluginParameter<String> myOutputFile = new PluginParameter.Builder<String>("o", null, String.class).guiName("Output File").required(true).outFile()
             .description("Output txt file that can be imported to Excel").build();
+    private PluginParameter<String> myTagFile = new PluginParameter.Builder<String>("tagFile", null, String.class).guiName("Tag File").required(false).inFile()
+            .description("Input file with list of tags to process.  Any number of tab-delimited columns, but must have 1 called Tags.").build();
 
     public GetTagTaxaDistFromDBPlugin() {
         super(null, false);
@@ -58,7 +63,6 @@ public class GetTagTaxaDistFromDBPlugin extends AbstractPlugin {
     public DataSet processData(DataSet input) {
         TagDataWriter tdw = new TagDataSQLite(inputDB());
         try {
-           //BufferedWriter fileWriter = null;
             BufferedWriter fileWriter = new BufferedWriter(new FileWriter(outputFile()));
             StringBuilder strB = new StringBuilder();
             TaxaList taxaList = tdw.getTaxaList(); // Used for printing taxon column headers
@@ -70,9 +74,36 @@ public class GetTagTaxaDistFromDBPlugin extends AbstractPlugin {
             strB.append("\n");
             fileWriter.write(strB.toString());
             strB.setLength(0);
-       
-            Set<Tag> myTags = tdw.getTags();
+
+            Set<Tag> myTags = new HashSet<Tag>();
+            if (tagFile() != null) {
+                // Will provide data only for user-supplied tags
+                BufferedReader tagFileReader = new BufferedReader(new FileReader(new File(tagFile())));
+                String[] headers1 =  tagFileReader.readLine().split("\t");
+                List<String> headers = new ArrayList<String>(Arrays.asList(headers1));
+                headers.replaceAll(String::toUpperCase); // allow user to have tags/Tags/TAGS etc
+                int tagColumn = headers.indexOf("TAGS");
+                if (tagColumn == -1) {
+                    tagFileReader.close();
+                    throw new IllegalArgumentException("Missing Tags column in tag file: " + tagFile());
+                }
+
+                String line = null;
+                // create list of Tag objects from the user provided file
+                while ((line = tagFileReader.readLine()) != null) {
+                    String tagSeq = line.split("\t")[tagColumn];
+                    Tag tag = TagBuilder.instance(tagSeq).build();
+                    myTags.add(tag);
+                }
+
+            } else {
+                // Data will be provided for all tags in the database
+                myTags = tdw.getTags();
+            }
+
+            // This code expects the user tag sequences to match those found in the db
             int tagcount = 0;
+            System.out.println("GetTagTaxaDist: number of tags to process: " + myTags.size());
             for (Tag myTag: myTags) {
                 tagcount++;
                 // get dist for each taxa
@@ -102,6 +133,9 @@ public class GetTagTaxaDistFromDBPlugin extends AbstractPlugin {
         return null;       
     }
 
+    public static void main(String[] args) {
+        GeneratePluginCode.generate(GetTagTaxaDistFromDBPlugin.class);
+    }
     @Override
     public ImageIcon getIcon() {
         // TODO Auto-generated method stub
@@ -110,14 +144,12 @@ public class GetTagTaxaDistFromDBPlugin extends AbstractPlugin {
 
     @Override
     public String getButtonName() {
-        // TODO Auto-generated method stub
-        return null;
+        return ("GetTagTaxaDistFromDB");
     }
 
     @Override
     public String getToolTipText() {
-        // TODO Auto-generated method stub
-        return null;
+        return ("From a given database, data on taxa distribution of tags is compiled and printed.");
     }
     /**
      * Input database file with tags and taxa distribution
@@ -164,5 +196,30 @@ public class GetTagTaxaDistFromDBPlugin extends AbstractPlugin {
         myOutputFile = new PluginParameter<>(myOutputFile, value);
         return this;
     }
+
+    /**
+     * Input file with list of tags to process.  Any number
+     * of tab-delimited columns, but must have 1 called Tags.
+     *
+     * @return Tag File
+     */
+    public String tagFile() {
+        return myTagFile.value();
+    }
+
+    /**
+     * Set Tag File. Input file with list of tags to process.
+     *  Any number of tab-delimited columns, but must have
+     * 1 called Tags.
+     *
+     * @param value Tag File
+     *
+     * @return this plugin
+     */
+    public GetTagTaxaDistFromDBPlugin tagFile(String value) {
+        myTagFile = new PluginParameter<>(myTagFile, value);
+        return this;
+    }
+
 
 }
