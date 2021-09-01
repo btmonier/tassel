baefb421402f8dc8d9d18ee9018f8311803334bc
Terry Casstevens <tmc46@cornell.edu>
2021-09-01 15:44:50 -0400

    Improved memory usage of TaxaList

--- src/net/maizegenetics/taxa/TaxaArrayList.java
+++ src/net/maizegenetics/taxa/TaxaArrayList.java
@@ -1,10 +1,10 @@
 package net.maizegenetics.taxa;
 
-import com.google.common.collect.ImmutableList;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import org.apache.log4j.Logger;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 
 /**
  * In memory immutable instance of {@link TaxaList}. Basic list of taxa
@@ -15,37 +15,17 @@ import java.util.*;
  * @author Ed Buckler
  * @author Terry Casstevens
  */
-class TaxaArrayList implements TaxaList {
+class TaxaArrayList extends TaxaNoIndexList {
 
     private static final Logger myLogger = Logger.getLogger(TaxaArrayList.class);
-    private final List<Taxon> myTaxaList;
-    private final int myNumTaxa;
     private final Map<String, Integer> myNameToIndex;
 
     TaxaArrayList(List<Taxon> srcList) {
-        myNumTaxa = srcList.size();
-        ImmutableList.Builder<Taxon> taxaListBuilder = ImmutableList.builder();
-        myNameToIndex = new Object2IntOpenHashMap(myNumTaxa);
-        for (int i = 0; i < myNumTaxa; i++) {
-            taxaListBuilder.add(srcList.get(i));
+        super(srcList);
+        myNameToIndex = new Object2IntOpenHashMap(numberOfTaxa());
+        for (int i = 0; i < numberOfTaxa(); i++) {
             myNameToIndex.put(srcList.get(i).getName(), i);
         }
-        myTaxaList = taxaListBuilder.build();
-    }
-
-    @Override
-    public int numberOfTaxa() {
-        return myNumTaxa;
-    }
-
-    @Override
-    public String taxaName(int index) {
-        return myTaxaList.get(index).getName();
-    }
-
-    @Override
-    public int size() {
-        return myNumTaxa;
     }
 
     @Override
@@ -53,16 +33,6 @@ class TaxaArrayList implements TaxaList {
         Integer index = myNameToIndex.get(name);
         if (index == null) return -1;
         return index;
-    }
-
-    @Override
-    public int indexOf(Taxon taxon) {
-        return indexOf(taxon.getName());
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return myTaxaList.isEmpty();
     }
 
     @Override
@@ -80,157 +50,4 @@ class TaxaArrayList implements TaxaList {
         }
     }
 
-    @Override
-    public Iterator<Taxon> iterator() {
-        return myTaxaList.iterator();
-    }
-
-    @Override
-    public Object[] toArray() {
-        return myTaxaList.toArray();
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return myTaxaList.toArray(a);
-    }
-
-    @Override
-    public boolean add(Taxon Taxon) {
-        throw new UnsupportedOperationException("This Class is Immutable.");
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        throw new UnsupportedOperationException("This Class is Immutable.");
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return myTaxaList.containsAll(c);
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends Taxon> c) {
-        throw new UnsupportedOperationException("This Class is Immutable.");
-    }
-
-    @Override
-    public boolean addAll(int index, Collection<? extends Taxon> c) {
-        throw new UnsupportedOperationException("This Class is Immutable.");
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        throw new UnsupportedOperationException("This Class is Immutable.");
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        throw new UnsupportedOperationException("This Class is Immutable.");
-    }
-
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException("This Class is Immutable.");
-    }
-
-    @Override
-    public Taxon get(int index) {
-        return myTaxaList.get(index);
-    }
-
-    @Override
-    public Taxon set(int index, Taxon element) {
-        throw new UnsupportedOperationException("This Class is Immutable.");
-    }
-
-    @Override
-    public void add(int index, Taxon element) {
-        throw new UnsupportedOperationException("This Class is Immutable.");
-    }
-
-    @Override
-    public Taxon remove(int index) {
-        throw new UnsupportedOperationException("This Class is Immutable.");
-    }
-
-    @Override
-    public int indexOf(Object o) {
-        return indexOf((Taxon) o);
-    }
-
-    @Override
-    public int lastIndexOf(Object o) {
-        return myTaxaList.lastIndexOf(o);
-    }
-
-    @Override
-    public ListIterator<Taxon> listIterator() {
-        return listIterator(0);
-    }
-
-    @Override
-    public ListIterator<Taxon> listIterator(final int index) {
-        return new ListIterator<Taxon>() {
-            private final ListIterator<Taxon> i = myTaxaList.listIterator(index);
-
-            public boolean hasNext() {
-                return i.hasNext();
-            }
-
-            public Taxon next() {
-                return i.next();
-            }
-
-            public boolean hasPrevious() {
-                return i.hasPrevious();
-            }
-
-            public Taxon previous() {
-                return i.previous();
-            }
-
-            public int nextIndex() {
-                return i.nextIndex();
-            }
-
-            public int previousIndex() {
-                return i.previousIndex();
-            }
-
-            public void remove() {
-                throw new UnsupportedOperationException();
-            }
-
-            public void set(Taxon e) {
-                throw new UnsupportedOperationException();
-            }
-
-            public void add(Taxon e) {
-                throw new UnsupportedOperationException();
-            }
-        };
-    }
-
-    @Override
-    public List<Taxon> subList(int fromIndex, int toIndex) {
-        return Collections.unmodifiableList(myTaxaList.subList(fromIndex, toIndex));
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof TaxaList)) return false;
-        TaxaList otherTaxa = (TaxaList) obj;
-        if (numberOfTaxa() != otherTaxa.numberOfTaxa()) return false;
-        Iterator<Taxon> myIter = myTaxaList.iterator();
-        Iterator<Taxon> otherIter = otherTaxa.iterator();
-        while (myIter.hasNext()) {
-            if ( !(myIter.next().equals(otherIter.next())) ) {
-                return false;
-            }
-        }
-        
-        return true;
-    }
 }
