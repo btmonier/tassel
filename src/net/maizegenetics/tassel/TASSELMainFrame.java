4d7d15c92abbe4eb3bd70c5ad476c7de10456e7e
Terry Casstevens <tmc46@cornell.edu>
2022-10-11 11:58:09 -0400

    New Build Date

--- src/net/maizegenetics/tassel/TASSELMainFrame.java
+++ src/net/maizegenetics/tassel/TASSELMainFrame.java
@@ -110,8 +110,8 @@ import java.util.Map;
 public class TASSELMainFrame extends JFrame implements ActionListener {
 
     private static final Logger myLogger = Logger.getLogger(TASSELMainFrame.class);
-    public static final String version = "5.2.85";
-    public static final String versionDate = "September 22, 2022";
+    public static final String version = "5.2.86";
+    public static final String versionDate = "October 11, 2022";
     private DataTreePanel myDataTreePanel;
     //a variable to control when the progress bar was last updated
     private JFileChooser filerSave = new JFileChooser();
