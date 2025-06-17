0c44c07c2003017fab1c95556631e99c2b3f1b40
Terry Casstevens <tmc46@cornell.edu>
2025-06-17 14:07:08 -0400

    Updates to TasselApp

--- tasselApp/src/wasmJsMain/kotlin/net/maizegenetics/ui/TasselWebApp.kt
+++ tasselApp/src/wasmJsMain/kotlin/net/maizegenetics/ui/TasselWebApp.kt
@@ -1,6 +1,7 @@
 package net.maizegenetics.ui
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -15,6 +16,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.painterResource
+import tassel.tasselapp.generated.resources.Res
+import tassel.tasselapp.generated.resources.genotype_viewer_example
 
 @Composable
 fun TasselWebApp() {
@@ -23,13 +27,18 @@ fun TasselWebApp() {
         Row(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp),
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .padding(10.dp)
+                .border(2.dp, Color.Gray, shape = RoundedCornerShape(10.dp)),
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.Top
         ) {
             leftColumn()
             Spacer(modifier = Modifier.width(10.dp))
-            exampleColumn()
+            // exampleColumn()
+            ResourceImageExample()
+            // rightColumn()
         }
 
     }
@@ -42,148 +51,154 @@ private fun leftColumn() {
             .safeContentPadding()
             .fillMaxWidth(0.25f)
             .fillMaxHeight()
-            .border(2.dp, Color.Red, shape = RoundedCornerShape(7.dp))
+            //.border(2.dp, Color.Red, shape = RoundedCornerShape(7.dp))
             .padding(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        datasetTree(modifier = Modifier.weight(0.45f))
+        datasetInfo(modifier = Modifier.weight(0.4f))
+        progressBar(modifier = Modifier.weight(0.15f))
+    }
+}
 
-        Row(
+@Composable
+private fun datasetTree(modifier: Modifier = Modifier) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.45f)
+            //.border(2.dp, Color.Red, shape = RoundedCornerShape(4.dp))
+            .padding(8.dp)
+    ) {
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .fillMaxHeight(0.45f)
-                .border(2.dp, Color.Red, shape = RoundedCornerShape(4.dp))
-                .padding(8.dp)
-                .weight(0.45f),
-            horizontalArrangement = Arrangement.SpaceBetween
+                .fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Column(
+            Text("Datasets")
+            Spacer(modifier = Modifier.height(5.dp))
+            DatasetTreeExample()
+        }
+    }
+}
+
+@Composable
+private fun datasetInfo(modifier: Modifier = Modifier) {
+
+    var text by remember {
+        mutableStateOf(
+            "Number of taxa: 281\n" +
+                    "Number of sites: 3093\n" +
+                    "\n" +
+                    "Chromosomes...\n" +
+                    "\n" +
+                    "1: 540 sites:\n" +
+                    "0 (157104) - 539 (299170077)\n" +
+                    "\n" +
+                    "2: 393 sites:\n" +
+                    "540 (736367) - 932 (234574991)\n" +
+                    "\n" +
+                    "3: 355 sites:\n" +
+                    "933 (1240310) - 1287 (229544509)\n" +
+                    "\n" +
+                    "4: 319 sites:\n" +
+                    "1288 (139753) - 1606 (245131801)\n" +
+                    "\n" +
+                    "5: 357 sites:\n" +
+                    "1607 (656148) - 1963 (216431558)\n" +
+                    "\n" +
+                    "6: 213 sites:\n" +
+                    "1964 (2379148) - 2176 (167883450)\n" +
+                    "\n" +
+                    "7: 246 sites:\n" +
+                    "2177 (729478) - 2422 (170346253)\n" +
+                    "\n" +
+                    "8: 256 sites:\n" +
+                    "2423 (169137) - 2678 (172323795)\n" +
+                    "\n" +
+                    "9: 213 sites:\n" +
+                    "2679 (3873116) - 2891 (151289948)\n" +
+                    "\n" +
+                    "10: 201 sites:\n" +
+                    "2892 (838970) - 3092 (148907116)\n" +
+                    "\n" +
+                    "\n" +
+                    "Nucleotide Codes\n" +
+                    "(Derived from IUPAC)...\n" +
+                    "A     A:A\n" +
+                    "C     C:C\n" +
+                    "G     G:G\n" +
+                    "T     T:T\n" +
+                    "R     A:G\n" +
+                    "Y     C:T\n" +
+                    "S     C:G\n" +
+                    "W     A:T\n" +
+                    "K     G:T\n" +
+                    "M     A:C\n" +
+                    "+     +:+ (insertion)\n" +
+                    "0     +:-\n" +
+                    "-     -:- (deletion)\n" +
+                    "N     Unknown\n" +
+                    "\n"
+        )
+    }
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.4f)
+            //.border(2.dp, Color.Red, shape = RoundedCornerShape(4.dp))
+            .padding(8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text("Dataset Description")
+            Spacer(modifier = Modifier.height(10.dp))
+            TextField(
+                value = text,
+                onValueChange = { /* no-op: ignore user edits */ },
+                readOnly = true,
+                //label = { Text("Dataset Description") },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .fillMaxHeight(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Text("Datasets")
-                Spacer(modifier = Modifier.height(10.dp))
-                DatasetTreeExample()
-            }
-        }
-
-        var text by remember {
-            mutableStateOf(
-                "Number of taxa: 281\n" +
-                        "Number of sites: 3093\n" +
-                        "\n" +
-                        "Chromosomes...\n" +
-                        "\n" +
-                        "1: 540 sites:\n" +
-                        "0 (157104) - 539 (299170077)\n" +
-                        "\n" +
-                        "2: 393 sites:\n" +
-                        "540 (736367) - 932 (234574991)\n" +
-                        "\n" +
-                        "3: 355 sites:\n" +
-                        "933 (1240310) - 1287 (229544509)\n" +
-                        "\n" +
-                        "4: 319 sites:\n" +
-                        "1288 (139753) - 1606 (245131801)\n" +
-                        "\n" +
-                        "5: 357 sites:\n" +
-                        "1607 (656148) - 1963 (216431558)\n" +
-                        "\n" +
-                        "6: 213 sites:\n" +
-                        "1964 (2379148) - 2176 (167883450)\n" +
-                        "\n" +
-                        "7: 246 sites:\n" +
-                        "2177 (729478) - 2422 (170346253)\n" +
-                        "\n" +
-                        "8: 256 sites:\n" +
-                        "2423 (169137) - 2678 (172323795)\n" +
-                        "\n" +
-                        "9: 213 sites:\n" +
-                        "2679 (3873116) - 2891 (151289948)\n" +
-                        "\n" +
-                        "10: 201 sites:\n" +
-                        "2892 (838970) - 3092 (148907116)\n" +
-                        "\n" +
-                        "\n" +
-                        "Nucleotide Codes\n" +
-                        "(Derived from IUPAC)...\n" +
-                        "A     A:A\n" +
-                        "C     C:C\n" +
-                        "G     G:G\n" +
-                        "T     T:T\n" +
-                        "R     A:G\n" +
-                        "Y     C:T\n" +
-                        "S     C:G\n" +
-                        "W     A:T\n" +
-                        "K     G:T\n" +
-                        "M     A:C\n" +
-                        "+     +:+ (insertion)\n" +
-                        "0     +:-\n" +
-                        "-     -:- (deletion)\n" +
-                        "N     Unknown\n" +
-                        "\n"
+                    .fillMaxHeight()
             )
         }
+    }
 
-        Row(
+}
+
+@Composable
+private fun progressBar(modifier: Modifier = Modifier) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.15f)
+            //.border(2.dp, Color.Red, shape = RoundedCornerShape(7.dp))
+            .padding(8.dp)
+    ) {
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .fillMaxHeight(0.45f)
-                .border(2.dp, Color.Red, shape = RoundedCornerShape(4.dp))
-                .padding(8.dp)
-                .weight(0.45f),
-            horizontalArrangement = Arrangement.SpaceBetween
+                .fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Column(
+            Text("Progress")
+            Spacer(modifier = Modifier.height(10.dp))
+            LinearProgressIndicator(
+                progress = {
+                    0.25f
+                },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .fillMaxHeight(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Spacer(modifier = Modifier.height(10.dp))
-                Text("Dataset Description")
-                Spacer(modifier = Modifier.height(10.dp))
-                TextField(
-                    value = text,
-                    onValueChange = { /* no-op: ignore user edits */ },
-                    readOnly = true,
-                    //label = { Text("Dataset Description") },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight()
-                )
-            }
+                    .height(10.dp)
+            )
         }
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.1f)
-                .fillMaxSize(0.1f)
-                .border(2.dp, Color.Red, shape = RoundedCornerShape(7.dp))
-                .padding(8.dp)
-                .weight(0.1f),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Spacer(modifier = Modifier.height(10.dp))
-                Text("Progress")
-                Spacer(modifier = Modifier.height(10.dp))
-                LinearProgressIndicator(
-                    progress = {
-                        0.5f
-                    },
-                    modifier = Modifier.fillMaxWidth(),
-                )
-            }
-        }
-
     }
 }
 
@@ -230,9 +245,9 @@ fun <T> TreeView(
 ) {
     Column(
         modifier = Modifier
-            .padding(16.dp)
+            .padding(8.dp)
             .fillMaxHeight()
-            .border(2.dp, Color.Red, shape = RoundedCornerShape(4.dp))
+        //.border(2.dp, Color.Red, shape = RoundedCornerShape(4.dp))
     ) {
         nodes.forEach { node ->
             TreeNodeView(node, 0, indent, nodeContent)
@@ -240,7 +255,6 @@ fun <T> TreeView(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun <T> TreeNodeView(
     node: TreeNode<T>,
@@ -318,3 +332,20 @@ private fun DatasetTreeExample() {
         Text(text = label, style = MaterialTheme.typography.bodyMedium)
     }
 }
+
+@Composable
+fun ResourceImageExample() {
+
+    Column(
+        modifier = Modifier
+            .safeContentPadding()
+            .fillMaxWidth(0.75f)
+            //.border(2.dp, Color.Red, shape = RoundedCornerShape(4.dp))
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(painterResource(Res.drawable.genotype_viewer_example), null)
+    }
+
+}
+
