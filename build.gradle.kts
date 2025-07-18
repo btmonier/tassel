7f1e57023ad684613244d18c632405a30275d344
Brandon <brandon.monier@gmail.com>
2025-07-18 18:59:43 -0500

    Fix signing attempt

--- build.gradle.kts
+++ build.gradle.kts
@@ -322,10 +322,10 @@ tasks.named("generateMetadataFileForMavenPublication") {
     dependsOn(tasks.named("dokkaJar"))
 }
 
-signing {
-    useInMemoryPgpKeys(System.getenv("ORG_GPG_SIGNING_KEY"), System.getenv("ORG_GPG_SIGNING_PASSWORD"))
-    sign(publishing.publications["maven"])
-}
+//signing {
+//    useInMemoryPgpKeys(System.getenv("ORG_GPG_SIGNING_KEY"), System.getenv("ORG_GPG_SIGNING_PASSWORD"))
+//    sign(publishing.publications["maven"])
+//}
 
 jreleaser {
     signing {
